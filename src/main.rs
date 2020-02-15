1450dd038cb82144b3238304837c4e9f9f9ef565
David Durst <davidbdurst@gmail.com>
2020-02-14 16:29:22 -0800

    moved to messing with lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,13 @@
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
-
 fn main() {
-    println!("Guess the number!");
+    let mut s = String::from("hello");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let r1 = &mut s;
+    println!("{}", r1);
 
-    loop {
-        println!("Please input your guess.");
+    change(s);
 
-        let mut guess = String::new();
+}
 
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            },
-        }
-    }
+fn change(mut some_string: String) {
+    some_string.push_str(", world");
 }
