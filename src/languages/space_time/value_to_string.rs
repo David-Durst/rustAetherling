8ee7be6bb96351588979e82edd544e747b20b2b5
David Durst <davidbdurst@gmail.com>
2020-02-16 23:38:30 -0800

    can convert seq values to flat lists

--- src/languages/space_time/value_to_string.rs
+++ src/languages/space_time/value_to_string.rs
@@ -1,14 +1,27 @@
 use std::fmt::Write;
+/*
 
-trait ToAtomStrings {
+    /// # Examples
+    /// ```
+    /// use aetherling::languages::space_time::value_to_string::ToAtomStrings;
+    ///
+    /// let mut builder: Vec<String> = Vec::new();
+    /// vec!(4,2,1,5).convert_to_flat_atom_list(&mut builder, true);
+    /// assert_eq!(builder, vec!("4", "2", "1", "5"))
+    /// ```
+*/
+pub trait ToAtomStrings {
     /// Convert an atom (or potentially nested Vec of atoms) to a 1D Vec of the atoms' string
     /// representations.
     /// The string vec argument stores the result.
-    fn convert_to_flat_atom_list(&self, builder: &mut Vec<String>);
+    ///
+    /// Call this with an empty `builder` and `top` as True, it will recur and
+    /// update those values
+    fn convert_to_flat_atom_list(&self, builder: &mut Vec<String>, top: bool);
 }
 
 impl ToAtomStrings for i32 {
-    fn convert_to_flat_atom_list(&self, builder: &mut Vec<String>) {
+    fn convert_to_flat_atom_list(&self, builder: &mut Vec<String>, _: bool) {
         match builder.last_mut() {
             Some(s) => write!(s, "{}", self),
             None => {
@@ -22,7 +35,7 @@ impl ToAtomStrings for i32 {
 }
 
 impl ToAtomStrings for bool {
-    fn convert_to_flat_atom_list(&self, builder: &mut Vec<String>) {
+    fn convert_to_flat_atom_list(&self, builder: &mut Vec<String>, _: bool) {
         match builder.last_mut() {
             Some(s) => write!(s, "{}", self),
             None => {
@@ -36,66 +49,78 @@ impl ToAtomStrings for bool {
 }
 
 impl<A: ToAtomStrings, B: ToAtomStrings> ToAtomStrings for (A,B) {
-    fn convert_to_flat_atom_list(&self, builder: &mut Vec<String>) {
-        let s = match builder.last_mut() {
+    fn convert_to_flat_atom_list(&self, builder: &mut Vec<String>, _: bool) {
+        // ensure builder isn't empty
+        match builder.last_mut() {
             Some(s) => s,
             None => {
-                let mut s = String::new();
-                builder.push(s);
+                builder.push(String::new());
                 // the compiler doesn't know this is safe, but I do
                 builder.last_mut().unwrap()
             }
         };
         let (a,b) = self;
-        write!("(", s);
-        a.convert_to_flat_atom_list(builder);
-        write!(",", s);
-        b.convert_to_flat_atom_list(builder);
-        write!(")", s);
+        // now I know builder isn't empty and I'm trusting
+        // that tuples are only of atoms.
+        write!(builder.last_mut().unwrap(), "(").unwrap();
+        a.convert_to_flat_atom_list(builder, false);
+        write!(builder.last_mut().unwrap(), ",").unwrap();
+        b.convert_to_flat_atom_list(builder, false);
+        write!(builder.last_mut().unwrap(), ")").unwrap();
     }
 }
 
-/*
-impl<A,B> ToAtomStrings for (A,B) {
-    fn convert_to_flat_atom_list(&self, builder: &mut Vec<String>) {
-        let s = match builder.last() {
-            Some(s) => s,
-            None => {
-                let s = String::new();
-                builder.push(s);
-                s
+impl<A: ToAtomStrings > ToAtomStrings for Vec<A> {
+    fn convert_to_flat_atom_list(&self, builder: &mut Vec<String>, top: bool) {
+        for (idx, elem) in self.iter().enumerate() {
+            // if this is the first element, only add a vec if this is the top vector
+            // otherwise on first index let parent vector create string
+            // always insert string otherwise
+            if (idx == 0 && top) || (idx > 0) {
+                builder.push(String::new())
             }
+            elem.convert_to_flat_atom_list(builder, false)
         }
     }
 }
-class Convertible_To_Atom_Strings a where
-  convert_to_flat_atom_list :: a -> ST_Val_To_String_Config -> [String]
 
-instance Convertible_To_Atom_Strings Integer where
-  convert_to_flat_atom_list x conf = [make_integer_string_for_backend conf x]
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-instance Convertible_To_Atom_Strings Bool where
-  convert_to_flat_atom_list x conf = [make_bool_string_for_backend conf x]
+    #[test]
+    fn test_convert_to_flat_atom_list_int() {
+        let mut builder: Vec<String> = Vec::new();
+        1.convert_to_flat_atom_list(&mut builder, true);
+        assert_eq!(builder, vec!("1"))
+    }
 
-instance (Convertible_To_Atom_Strings a, Convertible_To_Atom_Strings b) =>
-  Convertible_To_Atom_Strings (a, b) where
-  convert_to_flat_atom_list (x, y) conf =
-    [make_tuple_string_for_backend conf
-      (head $ convert_to_flat_atom_list x conf)
-      (head $ convert_to_flat_atom_list y conf)]
+    #[test]
+    fn test_convert_to_flat_atom_list_bool() {
+        let mut builder: Vec<String> = Vec::new();
+        true.convert_to_flat_atom_list(&mut builder, true);
+        assert_eq!(builder, vec!("true"))
+    }
 
-instance (Convertible_To_Atom_Strings a) => Convertible_To_Atom_Strings [a] where
-  convert_to_flat_atom_list xs conf = concat $
-    map (\x -> convert_to_flat_atom_list x conf) xs
+    #[test]
+    fn test_convert_to_flat_atom_list_tuple() {
+        let mut builder: Vec<String> = Vec::new();
+        (3, false).convert_to_flat_atom_list(&mut builder, true);
+        assert_eq!(builder, vec!("(3,false)"))
+    }
 
-instance Convertible_To_Atom_Strings AST_Atoms where
-  convert_to_flat_atom_list (BitA b) conf =
-    [make_bool_string_for_backend conf $ b]
-  convert_to_flat_atom_list (IntA i) conf =
-    [make_integer_string_for_backend conf i]
-  convert_to_flat_atom_list (ATupleA x y) conf =
-    [make_tuple_string_for_backend conf
-      (head $ convert_to_flat_atom_list x conf)
-      (head $ convert_to_flat_atom_list y conf)]
+    #[test]
+    fn test_convert_to_flat_atom_list_array() {
+        let mut builder: Vec<String> = Vec::new();
+        vec!(4,2,1,5).convert_to_flat_atom_list(&mut builder, true);
+        assert_eq!(builder, vec!("4", "2", "1", "5"))
+    }
 
-*/
+    #[test]
+    fn test_convert_to_flat_atom_list_nested_array() {
+        let mut builder: Vec<String> = Vec::new();
+        vec!(vec!(4,2),vec!(1,5)).convert_to_flat_atom_list(&mut builder, true);
+        assert_eq!(builder, vec!("4", "2", "1", "5"))
+    }
+}
+
