fca5fdc439583e53701683a00cfa88f56a9577d7
David Durst <davidbdurst@gmail.com>
2020-02-17 18:47:08 -0800

    convert seq val to st val string passes basic tests

--- src/languages/space_time/value_to_string/convert_seq_val_to_st_val_string.rs
+++ src/languages/space_time/value_to_string/convert_seq_val_to_st_val_string.rs
@@ -29,14 +29,36 @@ convert_seq_val_to_st_val_string seq_val st_type conf = do
 */
 
 pub fn convert_seq_val_to_st_val_string<T: ToAtomStrings, W: Write>(seq_val: T, st_type: Type, sink: &mut W) -> Result<(), Box<dyn Error>> {
-    let mut wtr = csv::Writer::from_writer(sink);
     let mut flat_val_strs: Vec<Rc<String>> = Vec::new();
     seq_val.convert_to_flat_atom_list(&mut flat_val_strs, true);
     let mut flat_val_idx_to_str: HashMap<usize, Rc<String>> = flat_val_strs.into_iter().enumerate().collect();
 
+    let total_width = st_type.atoms_per_valid();
+    let total_time = st_type.clocks();
     let st_vals = convert_seq_idxs_to_vals_to_time_space_vec(&mut flat_val_idx_to_str, st_type);
-    wtr.serialize(st_vals)?;
-    wtr.flush()?;
+
+    // write a csv array where only wrap the space dimension if it has more than 1 element
+    sink.write("[".as_ref())?;
+    for t in 0..total_time {
+        if t > 0 {
+            sink.write(",".as_ref())?;
+        }
+        if total_width == 1 {
+            sink.write(st_vals[t as usize][0].as_ref().as_bytes())?;
+        }
+        else {
+            sink.write("[".as_ref())?;
+            for s in 0..total_width {
+                sink.write(st_vals[t as usize][s as usize].as_ref().as_bytes())?;
+                if s < total_width - 1 {
+                    sink.write(",".as_ref())?;
+                }
+            }
+            sink.write("]".as_ref())?;
+        }
+    }
+    sink.write("]".as_ref())?;
+    sink.flush()?;
     Ok(())
 }
 
@@ -135,7 +157,27 @@ mod tests {
                                          Type::SSeq {n: 4, elem_type: Box::from(Type::Int)},
                                          &mut builder).unwrap();
         let data = String::from_utf8(builder).unwrap();
+        assert_eq!(data, String::from("[[1,3,2,4]]"))
+    }
+
+    #[test]
+    fn test_convert_seq_val_to_st_val_string_tseq_4_int() {
+        let mut builder = Vec::new();
+        convert_seq_val_to_st_val_string(vec!(1,3,2,4),
+                                         Type::TSeq {n: 4, i: 0, elem_type: Box::from(Type::Int)},
+                                         &mut builder).unwrap();
+        let data = String::from_utf8(builder).unwrap();
         assert_eq!(data, String::from("[1,3,2,4]"))
+    }
+
+    #[test]
+    fn test_convert_seq_val_to_st_val_string_tseq_2_1_int() {
+        let mut builder = Vec::new();
+        convert_seq_val_to_st_val_string(vec!(1,3),
+                                         Type::TSeq {n: 2, i: 1, elem_type: Box::from(Type::Int)},
+                                         &mut builder).unwrap();
+        let data = String::from_utf8(builder).unwrap();
+        assert_eq!(data, String::from("[1,3,0]"))
     }
 }
 /*
