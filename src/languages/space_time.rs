6f0e2d3af87a5375a289bf39b6fdb109a955d7c4
David Durst <davidbdurst@gmail.com>
2020-02-19 10:18:21 -0800

    load type compiles

--- src/languages/space_time.rs
+++ src/languages/space_time.rs
@@ -1,7 +1,19 @@
+
 pub mod ast;
 pub mod types;
 pub mod value_to_string;
 
 pub mod types_serialized {
-    include!("./types_serialized.rs");
+    include!(concat!(env!("OUT_DIR"), "/languages.space_time.types_serialized.rs"));
+}
+
+use std::io::Read;
+use std::io::Cursor;
+use types::Type;
+use prost::Message;
+pub fn load_type<T: Read + AsRef<[u8]>>(src: &mut T) -> Type {
+    let derialized_type = types_serialized::TypeSerialized::decode(&mut Cursor::new(src))
+        .unwrap();
+    println!("The loaded struct is: {:?}", derialized_type);
+    unimplemented!()
 }
