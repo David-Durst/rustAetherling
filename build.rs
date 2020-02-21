a78f53afb23a8ff32dcb16e02e5dbb33d874d8fe
David Durst <davidbdurst@gmail.com>
2020-02-21 01:05:13 -0800

    moved serialize in space_tiem to own subpackage

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    prost_build::compile_protos(&["protoAetherling/spacetime.proto"],
+    prost_build::compile_protos(&["protoAetherling/spacetime.proto", "protoAetherling/sequence.proto"],
                                 &["protoAetherling"]).unwrap();
 }
